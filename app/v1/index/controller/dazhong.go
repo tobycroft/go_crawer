783b72afb4c20f864869c06b18983b69c43855af
火线兔 <silverhawk@foxmail.com>
2023-04-13 14:39:27 +0800

    Idea

--- app/v1/index/controller/dazhong.go
+++ app/v1/index/controller/dazhong.go
@@ -1,8 +1,12 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gocolly/colly/v2"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+	"regexp"
 )
 
 func DazhongController(route *gin.RouterGroup) {
@@ -16,5 +20,36 @@ type MtCraw struct {
 }
 
 func dazhong_1(c *gin.Context) {
+	body, err := c.GetRawData()
+	if err != nil {
+		fmt.Println(err)
+	}
+	html := string(body)
+	re := regexp.MustCompile(`<a .*? data-shopid="(.*?)" .*? title="(.*?)" .*? href="(.*?)">`)
+	matchess := re.FindAllStringSubmatch(html, -1)
+
+	for _, matches := range matchess {
+		if len(matches) > 0 {
+			shopid := matches[1]
+			title := matches[2]
+			href := matches[3]
+			data := map[string]any{
+				"name":   title,
+				"shopid": shopid,
+				"url":    href,
+			}
+			db := tuuz.Db().Table("c_dazhong_xuexipeixun")
+			thedata, err := db.Where("name", title).Find()
+			if err != nil {
+				Log.Dbrr(err, tuuz.FUNCTION_ALL())
+			}
+			if len(thedata) < 1 {
+				tuuz.Db().Table("c_dazhong_xuexipeixun").Data(data).Insert()
+				fmt.Println(shopid, title, href)
+			}
+		} else {
+			fmt.Println("No match found")
+		}
+	}
 
 }

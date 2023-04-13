34978f44ed89123846e7d6ebca6de78dea2da91d
火线兔 <silverhawk@foxmail.com>
2023-04-13 15:01:38 +0800

    Idea

--- app/v1/index/controller/dazhong.go
+++ app/v1/index/controller/dazhong.go
@@ -19,6 +19,8 @@ type MtCraw struct {
 	maxid int64
 }
 
+const Table = "c_dazhong_xuexipeixun"
+
 func dazhong_1(c *gin.Context) {
 	body, err := c.GetRawData()
 	if err != nil {
@@ -27,29 +29,64 @@ func dazhong_1(c *gin.Context) {
 	html := string(body)
 	re := regexp.MustCompile(`<a .*? data-shopid="(.*?)" .*? title="(.*?)" .*? href="(.*?)">`)
 	matchess := re.FindAllStringSubmatch(html, -1)
-
-	for _, matches := range matchess {
-		if len(matches) > 0 {
-			shopid := matches[1]
-			title := matches[2]
-			href := matches[3]
-			data := map[string]any{
-				"name":   title,
-				"shopid": shopid,
-				"url":    href,
+	if len(matchess) > 0 {
+		for _, matches := range matchess {
+			if len(matches) > 0 {
+				shopid := matches[1]
+				title := matches[2]
+				href := matches[3]
+				data := map[string]any{
+					"name":   title,
+					"shopid": shopid,
+					"url":    href,
+				}
+				db := tuuz.Db().Table(Table)
+				thedata, err := db.Where("name", title).Find()
+				if err != nil {
+					Log.Dbrr(err, tuuz.FUNCTION_ALL())
+				}
+				if len(thedata) < 1 {
+					tuuz.Db().Table(Table).Data(data).Insert()
+					fmt.Println(shopid, title, href)
+				}
+			} else {
+				fmt.Println("No match found")
 			}
-			db := tuuz.Db().Table("c_dazhong_xuexipeixun")
-			thedata, err := db.Where("name", title).Find()
-			if err != nil {
-				Log.Dbrr(err, tuuz.FUNCTION_ALL())
-			}
-			if len(thedata) < 1 {
-				tuuz.Db().Table("c_dazhong_xuexipeixun").Data(data).Insert()
-				fmt.Println(shopid, title, href)
-			}
-		} else {
-			fmt.Println("No match found")
 		}
+	} else {
+		//fmt.Println(html)
+		re := regexp.MustCompile(`\/shop\/(\w+)\/review`)
+		match := re.FindStringSubmatch(html)
+		if len(match) > 1 {
+			shopid := match[1]
+			re = regexp.MustCompile(`<div class="address">\s+<span class="item">地址：</span>\s+(.*?)\s+</div>`)
+
+			match := re.FindStringSubmatch(html)
+			address := ""
+			phone := ""
+			if len(match) > 1 {
+				address = match[1]
+			} else {
+				fmt.Println("address No match found")
+				return
+			}
+
+			re = regexp.MustCompile(`data-phone="(\d+-\d+)"`)
+			result := re.FindStringSubmatch(html)
+
+			if len(result) == 2 {
+				phone = result[1]
+			} else {
+				fmt.Println("phone No match found.")
+				return
+			}
+			tuuz.Db().Table(Table).Where("shopid", shopid).Data(map[string]any{
+				"address": address,
+				"phone":   phone,
+			})
+		}
+		//re := regexp.MustCompile(`<div class="address">\s+<span class="item">地址：</span>\s+(.*?)\s+</div>`)
+
 	}
 
 }
